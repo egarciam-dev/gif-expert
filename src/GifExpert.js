2edc6efe26c578681d34aa89f771b22cc8677c9a
Engel <engel.garcia7@gmail.com>
2022-05-26 11:39:06 -0400

    init

---
+++ src/GifExpert.js
@@ -0,0 +1,12 @@
+// Created this componet using rafc snippet
+
+import React from 'react'
+
+export const GifExpert = () => {
+  return (
+      <>
+        <h2>GifExpert</h2>
+        <hr />
+      </>
+  )
+}
