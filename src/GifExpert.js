659cd8aa6ed230c913b6570f3db94fea322d33e5
Engel <engel.garcia7@gmail.com>
2022-05-26 14:00:57 -0400

    commit

--- src/GifExpert.js
+++ src/GifExpert.js
@@ -1,12 +1,34 @@
 // Created this componet using rafc snippet
 
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpert = () => {
+
+   // const categories = ['One Punch Man', 'Spider Man', 'Goku']
+
+    const [categories, setCategories] = useState(['One Punch Man', 'Spider Man', 'Goku']);
+
+    const handleAdd = (category) => {
+        //setCategories( [...categories, 'Naruto'] );
+        setCategories( cats => [ ...cats, 'Naruto']);
+    }
+
   return (
       <>
         <h2>GifExpert</h2>
         <hr />
+
+        <button onClick={ handleAdd } >Agregar categoría</button>
+
+        <ul>
+            { categories.map( category => {
+                return <li key={category}>{category}</li>
+            } ) }
+        </ul> 
+
       </>
+
   )
+
+
 }
